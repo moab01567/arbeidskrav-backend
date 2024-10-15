9ff31f78c8ee93f5dd172ccf8d8f35476b06e768
Mohemmed <moh080203@gmail.com>
2024-10-15 15:08:10 +0200

    commit init

--- src/main/java/no/moab015/BackendBicycles/BackendBicyclesApplication.java
+++ src/main/java/no/moab015/BackendBicycles/BackendBicyclesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendBicyclesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendBicyclesApplication.class, args);
 	}
-
 }
