68098507f46d1ecbd7355880d9eb50b3c12334fc
Mohemmed <moh080203@gmail.com>
2024-10-15 15:04:07 +0200

    commit init

---
+++ src/main/java/no/moab015/BackendBicycles/BackendBicyclesApplication.java
@@ -0,0 +1,13 @@
+package no.moab015.BackendBicycles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBicyclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBicyclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/moab015/BackendBicycles/BackendBicyclesApplicationTests.java
@@ -0,0 +1,13 @@
+package no.moab015.BackendBicycles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBicyclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
