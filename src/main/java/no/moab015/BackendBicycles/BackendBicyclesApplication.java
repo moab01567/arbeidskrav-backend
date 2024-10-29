ae203a67e145bf41df211af9014c2dbecfd21a52
moab01567 <moab015@student.kristianina.no>
2024-10-29 21:21:27 +0100

    all good, just a new line :) nothing new

--- src/main/java/no/moab015/BackendBicycles/BackendBicyclesApplication.java
+++ src/main/java/no/moab015/BackendBicycles/BackendBicyclesApplication.java
@@ -3,6 +3,7 @@ package no.moab015.BackendBicycles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BackendBicyclesApplication {
 	public static void main(String[] args) {
